cb5945c17fb803913b70cf572b7f4ef7f4f81fe0
Marlena Janasz <mar.janasz87@gmail.com>
2019-06-03 11:43:25 +0200

    adding optimised js excercise

--- app.js
+++ app.js
@@ -1,214 +1,32 @@
+onst blackBoxes = document.querySelectorAll(".blackbox");
+
+blackBoxes.forEach(function(blackBox){
+
+	let color = function(){
+	blackBox.style.backgroundColor = "purple";	
+	};
+	blackBox.addEventListener("click", color);
+	blackBox.addEventListener("dblclick", function(){
+	if(color = true){
+		blackBox.style.backgroundColor = "black";	
+	};
+});
+
+});
+
+
+//
 const head2 = document.querySelector("h2.sol3");
-head2.addEventListener("click", function(){           //lepiej przypisać funkcję do zmiennej
+head2.addEventListener("click", function(){         
 	const div = document.createElement("div");
 	div.setAttribute("class", "container");
 	document.body.appendChild(div);
-	const innerDiv = document.createElement("div");
-	innerDiv.setAttribute("class", "item");
-	div.appendChild(innerDiv);
-	const innerDiv2 = document.createElement("div");
-	innerDiv2.setAttribute("class", "item");
-	div.appendChild(innerDiv2);
-	const innerDiv3 = document.createElement("div");
-	innerDiv3.setAttribute("class", "item");
-	div.appendChild(innerDiv3);
-	const innerDiv4 = document.createElement("div");
-	innerDiv4.setAttribute("class", "item");
-	div.appendChild(innerDiv4);
-	const innerDiv5 = document.createElement("div");
-	innerDiv5.setAttribute("class", "item");
-	div.appendChild(innerDiv5);
-	const innerDiv6 = document.createElement("div");
-	innerDiv6.setAttribute("class", "item");
-	div.appendChild(innerDiv6);
-	const innerDiv7 = document.createElement("div");
-	innerDiv7.setAttribute("class", "item");
-	div.appendChild(innerDiv7);
-	const innerDiv8 = document.createElement("div");
-	innerDiv8.setAttribute("class", "item");
-	div.appendChild(innerDiv8);
-	const innerDiv9 = document.createElement("div");
-	innerDiv9.setAttribute("class", "item");
-	div.appendChild(innerDiv9);
-	const innerDiv10 = document.createElement("div");
-	innerDiv10.setAttribute("class", "item");
-	div.appendChild(innerDiv10);
-	const innerDiv11 = document.createElement("div");
-	innerDiv11.setAttribute("class", "item");
-	div.appendChild(innerDiv11);
-	const innerDiv12 = document.createElement("div");
-	innerDiv12.setAttribute("class", "item");
-	div.appendChild(innerDiv12);
-	const innerDiv13 = document.createElement("div");
-	innerDiv13.setAttribute("class", "item");
-	div.appendChild(innerDiv13);
-	const innerDiv14 = document.createElement("div");
-	innerDiv14.setAttribute("class", "item");
-	div.appendChild(innerDiv14);
-	const innerDiv15 = document.createElement("div");
-	innerDiv15.setAttribute("class", "item");
-	div.appendChild(innerDiv15);
-	const innerDiv16 = document.createElement("div");
-	innerDiv16.setAttribute("class", "item");
-	div.appendChild(innerDiv16);
-	const innerDiv17 = document.createElement("div");
-	innerDiv17.setAttribute("class", "item");
-	div.appendChild(innerDiv17);
-	const innerDiv18 = document.createElement("div");
-	innerDiv18.setAttribute("class", "item");
-	div.appendChild(innerDiv18);
-	const innerDiv19 = document.createElement("div");
-	innerDiv19.setAttribute("class", "item");
-	div.appendChild(innerDiv19);
-	const innerDiv20 = document.createElement("div");
-	innerDiv20.setAttribute("class", "item");
-	div.appendChild(innerDiv20);
-	const innerDiv21 = document.createElement("div");
-	innerDiv21.setAttribute("class", "item");
-	div.appendChild(innerDiv21);
-	const innerDiv22 = document.createElement("div");
-	innerDiv22.setAttribute("class", "item");
-	div.appendChild(innerDiv22);
-	const innerDiv23 = document.createElement("div");
-	innerDiv23.setAttribute("class", "item");
-	div.appendChild(innerDiv23);
-	const innerDiv24 = document.createElement("div");
-	innerDiv24.setAttribute("class", "item");
-	div.appendChild(innerDiv24);
-	const innerDiv25 = document.createElement("div");
-	innerDiv25.setAttribute("class", "item");
-	div.appendChild(innerDiv25);
-});
-let color = function(){
-	blackbox1.style.backgroundColor = "purple";	
-};
-const blackbox1 = document.querySelector(".one");
-blackbox1.addEventListener("click", color);
-blackbox1.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox1.style.backgroundColor = "black";	
-	}
-})
+	
 
-let color2 = function(){
-	blackbox2.style.backgroundColor = "purple";	
-};
-const blackbox2 = document.querySelector(".two");
-blackbox2.addEventListener("click", color2);
-blackbox2.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox2.style.backgroundColor = "black";	
+	for(let i=0; i<25; i++){
+		document.querySelector("div");
+		const innerDiv = document.createElement("div");
+		innerDiv.setAttribute("class", "item");
+		div.appendChild(innerDiv);
 	}
-})
-let color3 = function(){
-	blackbox3.style.backgroundColor = "purple";	
-};
-const blackbox3 = document.querySelector(".three");
-blackbox3.addEventListener("click", color3);
-blackbox3.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox3.style.backgroundColor = "black";	
-	}
-})
-let color4 = function(){
-	blackbox4.style.backgroundColor = "purple";	
-};
-const blackbox4 = document.querySelector(".four");
-blackbox4.addEventListener("click", color4);
-blackbox4.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox4.style.backgroundColor = "black";	
-	}
-})
-let color5 = function(){
-	blackbox5.style.backgroundColor = "purple";	
-};
-const blackbox5 = document.querySelector(".five");
-blackbox5.addEventListener("click", color5);
-blackbox5.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox5.style.backgroundColor = "black";	
-	}
-})
-let color6 = function(){
-	blackbox6.style.backgroundColor = "purple";	
-};
-const blackbox6 = document.querySelector(".six");
-blackbox6.addEventListener("click", color6);
-blackbox6.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox6.style.backgroundColor = "black";	
-	}
-})
-let color7 = function(){
-	blackbox7.style.backgroundColor = "purple";	
-};
-const blackbox7 = document.querySelector(".seven");
-blackbox7.addEventListener("click", color7);
-blackbox7.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox7.style.backgroundColor = "black";	
-	}
-})
-let color8 = function(){
-	blackbox8.style.backgroundColor = "purple";	
-};
-const blackbox8 = document.querySelector(".eight");
-blackbox8.addEventListener("click", color8);
-blackbox8.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox8.style.backgroundColor = "black";	
-	}
-})
-let color9 = function(){
-	blackbox9.style.backgroundColor = "purple";	
-};
-const blackbox9 = document.querySelector(".nine");
-blackbox9.addEventListener("click", color9);
-blackbox9.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox9.style.backgroundColor = "black";	
-	}
-})
-
-let color10 = function(){
-	blackbox10.style.backgroundColor = "purple";	
-};
-const blackbox10 = document.querySelector(".ten");
-blackbox10.addEventListener("click", color10);
-blackbox10.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox10.style.backgroundColor = "black";	
-	}
-})
-
-let color11 = function(){
-	blackbox11.style.backgroundColor = "purple";	
-};
-const blackbox11 = document.querySelector(".eleven");
-blackbox11.addEventListener("click", color11);
-blackbox11.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox11.style.backgroundColor = "black";	
-	}
-})
-let color12 = function(){
-	blackbox12.style.backgroundColor = "purple";	
-};
-const blackbox12 = document.querySelector(".twelve");
-blackbox12.addEventListener("click", color12);
-blackbox12.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox12.style.backgroundColor = "black";	
-	}
-})
-let color13 = function(){
-	blackbox13.style.backgroundColor = "purple";	
-};
-const blackbox13 = document.querySelector(".thirteen");
-blackbox13.addEventListener("click", color13);
-blackbox13.addEventListener("dblclick", function(){
-	if(color = true){
-		blackbox13.style.backgroundColor = "black";	
-	}
-})
+	});
